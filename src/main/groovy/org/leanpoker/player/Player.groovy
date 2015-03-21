8dc931cfecdad62d57555d55ddc426e0f32e7d75
Bence Kiglics <bence.kiglics@prezi.com>
2015-03-21 09:51:22 +0100

    Putting input to logs

--- src/main/groovy/org/leanpoker/player/Player.groovy
+++ src/main/groovy/org/leanpoker/player/Player.groovy
@@ -5,6 +5,7 @@ class Player {
     static final String VERSION = 'Default Groovy folding player';
 
     static int betRequest(def gameState) {
+        print gameState
         Math.floor(Math.random() * 1000) as Integer
     }
 
